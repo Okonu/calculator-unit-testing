0f8fb131dccea2621a894f2f13fc8c8333b25c7f
Okonu <ianyakundi015@gmail.com>
2022-12-02 12:07:05 +0300

    calculator form

---
+++ app/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Claculator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    </script>
+    <link rel="stylesheet" href="sytle.css">
+</head>
+
+<body>
+    <div class="container" style="margin-top: 50px">
+
+        <!---calculator form -->
+
+        <form method="post">
+            <input name="number1" type="text" class="form-control inputField" />
+            <select name="operation">
+                <option value="plus">Plus</option>
+                <option value="Minus">Minus</option>
+                <option value="times">Times</option>
+                <option value="divided by">Divided by</option>
+            </select>
+        </form>
+
+
+    </div>
+</body>
+
+</html>
