6d182485bc1c654f04f8d6352d7fc62794b7085d
Okonu <ianyakundi015@gmail.com>
2022-12-06 00:16:01 +0300

    data output, error message

--- app/calculatorController.php
+++ app/calculatorController.php
@@ -7,16 +7,23 @@ if (isset($_POST['submit'])) {
     //checking number values
     if (is_numeric($_POST['number1']) && is_numeric($_POST['number2'])) {
         $calculator = new \App\Calculator;
-    }
 
-    //calculating totals
-    if ($_POST['operation'] == 'plus') {
-        $total = $calculator->add($_POST['number1'], $_POST['number2']);
-    } else if ($_POST['operation'] == 'minus') {
-        $total = $calculator->subtract($_POST['number1'], $_POST['number2']);
-    } else if ($_POST['operation'] == 'times') {
-        $total = $calculator->multiply($_POST['number1'], $_POST['number2']);
-    } else if ($_POST['operation'] == 'divided by') {
-        $total = $calculator->divide($_POST['number1'], $_POST['number2']);
+        //calculating totals
+        if ($_POST['operation'] == 'plus') {
+            $total = $calculator->add($_POST['number1'], $_POST['number2']);
+        } else if ($_POST['operation'] == 'minus') {
+            $total = $calculator->subtract($_POST['number1'], $_POST['number2']);
+        } else if ($_POST['operation'] == 'times') {
+            $total = $calculator->multiply($_POST['number1'], $_POST['number2']);
+        } else if ($_POST['operation'] == 'divided by') {
+            $total = $calculator->divide($_POST['number1'], $_POST['number2']);
+        }
+
+        //
+        echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals{$total}</h1>";
+    } else {
+
+        // Print error messsage to the browser
+        echo 'Numeric values are required for this operation';
     }
 }
